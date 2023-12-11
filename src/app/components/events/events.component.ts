676ada4d80be2b5e590e7c7a0cf9ebef0b79541a
leo <leonardosantos300305@gmail.com>
2023-12-11 16:08:24 -0300

    trabalhando com eventos do angular

---
+++ src/app/components/events/events.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-events',
+  templateUrl: './events.component.html',
+  styleUrls: ['./events.component.scss']
+})
+export class EventsComponent implements OnInit {
+
+  show: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  showMessage(): void {
+    //this.show = true;
+    this.show = !this.show; // toggle - trabalha com o inverso
+  }
+
+}
